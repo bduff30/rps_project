91330cf33c1bd9239be285ebcc65c077d940cab9
Brendan Duffy <bpduffy1997@gmail.com>
2022-06-28 05:26:24 -0400

    modified game so player chooses on button selection rather than prompt.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let hand = ["Rock", "Paper", "Scissors"];
-let playerHand = "Paper";
+let result = document.querySelector(".result");
+let playerWins = 0;
+let computerWins = 0;
+let score = document.querySelector(".score");
 
 function computerPlay() {
   let randomIndex = Math.floor(Math.random() * 3);
@@ -14,22 +17,51 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection == "Paper" && computerSelection == "Rock")
   ) {
     console.log("You Win!");
+    result.textContent = "You Win!";
+    playerWins++;
+    console.log("Player wins: " + playerWins);
   } else if (
     (playerSelection == "Rock" && computerSelection == "Paper") ||
     (playerSelection == "Paper" && computerSelection == "Scissors") ||
     (playerSelection == "Scissors" && computerSelection == "Rock")
   ) {
     console.log("You Lose!");
+    result.textContent = "You Lose!";
+    computerWins++;
+    console.log("Computer wins: " + computerWins);
   } else if (playerSelection == computerSelection) {
     console.log("You Tied!");
+    result.textContent = "You Tied!";
   } else {
     console.log("Error");
+    result.textContent = "Error!";
   }
+  if (playerWins == 5) {
+    result.textContent = "GAME OVER. PLAYER HAS WON";
+    playerWins = 0;
+    computerWins = 0;
+  } else if (computerWins == 5) {
+    result.textContent = "GAME OVER. COMPUTER HAS WON";
+    playerWins = 0;
+    computerWins = 0;
+  }
+  score.textContent = `Player Score:${playerWins} Computer Score:${computerWins} `;
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound(playerHand, computerPlay());
-  }
-}
-game();
+//Rock button chosen
+let computerRock = document.querySelector(".rockBtn");
+computerRock.addEventListener("click", () => {
+  playRound("Rock", computerPlay());
+});
+
+//Paper button chosen
+let computerPaper = document.querySelector(".paperBtn");
+computerPaper.addEventListener("click", () => {
+  playRound("Paper", computerPlay());
+});
+
+//Scissors button chosen
+let computerScissors = document.querySelector(".scissorsBtn");
+computerScissors.addEventListener("click", () => {
+  playRound("Scissors", computerPlay());
+});
